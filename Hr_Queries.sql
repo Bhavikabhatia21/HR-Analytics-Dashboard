8956a1824c5cf8e3afab5a0f542028352d29bac3
Bhavika Bhatia <129063862+Bhavikabhatia21@users.noreply.github.com>
2026-07-25 11:10:09 +0530

    Add files via upload

---
+++ Hr_Queries.sql
@@ -0,0 +1,17 @@
+CREATE TABLE hr_data (
+    Age INT,
+    Attrition VARCHAR(10),
+    BusinessTravel VARCHAR(50),
+    Department VARCHAR(50),
+    DistanceFromHome INT,
+    Education INT,
+    EducationField VARCHAR(50),
+    Gender VARCHAR(10),
+    JobRole VARCHAR(100),
+    MaritalStatus VARCHAR(20),
+    MonthlyIncome INT,
+    OverTime VARCHAR(10),
+    PercentSalaryHike INT,
+    TotalWorkingYears INT,
+    YearsAtCompany INT
+);
